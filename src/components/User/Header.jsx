9f5624827d7f6e2664b88a3cd9bf87287d33fd18
trung2004official <trung2004tech@gmail.com>
2025-06-29 09:13:35 +0700

    dynamic-user-header

--- src/components/User/Header.jsx
+++ src/components/User/Header.jsx
@@ -3,12 +3,16 @@ import { Link, useNavigate } from 'react-router-dom';
 import logo from '../../assets/quiztech_logo.svg';
 import { FaChevronDown } from 'react-icons/fa';
 import Swal from 'sweetalert2';
+import { jwtDecode } from 'jwt-decode';
 
 const Header = () => {
     // const [username, setUsername] = useState('guest'); // Giá trị mặc định
     const [showDropdown, setShowDropdown] = useState(false);
     const dropdownRef = useRef(null);
     const navigate = useNavigate();
+    const [user, setUser] = useState();
+    const token = localStorage.getItem('token');
+    const decodedToken =  token? jwtDecode(token) : null;
 
     useEffect(() => {
         const handleClickOutside = (event) => {
@@ -43,36 +47,6 @@ const Header = () => {
         })
     }
 
-    // useEffect(() => {
-    //     // Lấy username từ localStorage khi component mount
-    //     const storedUser = localStorage.getItem('currentUser');
-    //     if (storedUser) {
-    //         const parsedUser = JSON.parse(storedUser);
-    //         setUsername(parsedUser.username || 'guest');
-    //     }
-    // }, []);
-
-    // // Hàm đăng xuất
-    // const handleLogout = () => {
-    //     localStorage.removeItem('token');
-    //     setUsername('guest');
-    // };
-
-    // // Lấy thời gian hiện tại
-    // const getCurrentTime = () => {
-    //     const now = new Date();
-    //     return now.toLocaleTimeString('vi-VN', { timeZone: 'Asia/Ho_Chi_Minh', hour12: true });
-    // };
-
-    // const [currentTime, setCurrentTime] = useState(getCurrentTime());
-
-    // useEffect(() => {
-    //     const timer = setInterval(() => {
-    //         setCurrentTime(getCurrentTime());
-    //     }, 1000); // Cập nhật mỗi giây
-    //     return () => clearInterval(timer); // Dọn dẹp khi unmount
-    // }, []);
-
     return (
         <header className="bg-CetaceanBlue px-10 flex justify-between items-center py-4">
             <Link to="/home" className="flex items-center space-x-2">
@@ -91,40 +65,49 @@ const Header = () => {
                         </Link>
                     </li>
                 </ul>
-                <div className="relative" ref={dropdownRef}>
+                {decodedToken ? (
+                    <div className="relative" ref={dropdownRef}>
+                        <button
+                            className="flex items-center gap-2 focus:outline-none"
+                            onClick={() => setShowDropdown((prev) => !prev)}
+                        >
+                            <img
+                                className="w-12 h-12 rounded-full border-2 border-white"
+                                src="https://scontent.fsgn8-4.fna.fbcdn.net/v/t39.30808-6/506060153_2526619104351592_4052337228407427246_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeELRC_MLNJsOgR8-KY9wi0bEESlSnzrVuoQRKVKfOtW6oNuYorF16SWtiZpT2z_tYNaWC3-HqbEOuxzzSHIjtZd&_nc_ohc=PqBbrv-qrdUQ7kNvwF3bNpL&_nc_oc=Adla-YJoWubWOJ03UCIa14MjOGjAnlLiA5tegbI7TaGm82FvIG2yheUQ-uKBkom6T7kToz6pFfioAF-a1_gds4u8&_nc_zt=23&_nc_ht=scontent.fsgn8-4.fna&_nc_gid=2hjPd25YM-jRDTUxzHsRUw&oh=00_AfOmtFsThAqL751XGjIvZBX8bbCw1XMiC-KgDvdIS9St6Q&oe=6864ADA0"
+                                alt=""
+                            />
+                        </button>
+                        {showDropdown && (
+                            <div className="absolute right-0 mt-2 w-48 bg-PurpleNavy rounded shadow-lg z-50">
+                                <button
+                                    className="block w-full text-left px-4 py-2 hover:bg-CetaceanBlue-light hover:text-white transition"
+                                    onClick={() => { setShowDropdown(false); navigate('/profile'); }}
+                                >
+                                    Thông tin cá nhân
+                                </button>
+                                <button
+                                    className="block w-full text-left px-4 py-2 hover:bg-CetaceanBlue-light hover:text-white transition"
+                                    onClick={() => { setShowDropdown(false); /* navigate('/settings'); */ }}
+                                >
+                                    Cài đặt
+                                </button>
+                                <button
+                                    className="block w-full text-left px-4 py-2 text-red-500 hover:bg-CetaceanBlue-light transition"
+                                    onClick={() => handleClickLogOut()}
+                                >
+                                    Đăng xuất
+                                </button>
+                            </div>
+                        )}
+                    </div>
+                ) : (
                     <button
-                        className="flex items-center gap-2 focus:outline-none"
-                        onClick={() => setShowDropdown((prev) => !prev)}
+                        className="text-white px-4 py-2 rounded-lg  hover:text-CetaceanBlue-light transition duration-200"
+                        onClick={() => navigate('/login')}
                     >
-                        <img
-                            className="w-12 h-12 rounded-full border-2 border-white"
-                            src="https://scontent.fsgn8-4.fna.fbcdn.net/v/t39.30808-6/506060153_2526619104351592_4052337228407427246_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeELRC_MLNJsOgR8-KY9wi0bEESlSnzrVuoQRKVKfOtW6oNuYorF16SWtiZpT2z_tYNaWC3-HqbEOuxzzSHIjtZd&_nc_ohc=PqBbrv-qrdUQ7kNvwF3bNpL&_nc_oc=Adla-YJoWubWOJ03UCIa14MjOGjAnlLiA5tegbI7TaGm82FvIG2yheUQ-uKBkom6T7kToz6pFfioAF-a1_gds4u8&_nc_zt=23&_nc_ht=scontent.fsgn8-4.fna&_nc_gid=2hjPd25YM-jRDTUxzHsRUw&oh=00_AfOmtFsThAqL751XGjIvZBX8bbCw1XMiC-KgDvdIS9St6Q&oe=6864ADA0"
-                            alt=""
-                        />
+                        Đăng nhập
                     </button>
-                    {showDropdown && (
-                        <div className="absolute right-0 mt-2 w-48 bg-PurpleNavy rounded shadow-lg z-50">
-                            <button
-                                className="block w-full text-left px-4 py-2 hover:bg-CetaceanBlue-light hover:text-white transition"
-                                onClick={() => { setShowDropdown(false); navigate('/profile'); }}
-                            >
-                                Thông tin cá nhân
-                            </button>
-                            <button
-                                className="block w-full text-left px-4 py-2 hover:bg-CetaceanBlue-light hover:text-white transition"
-                                onClick={() => { setShowDropdown(false); /* navigate('/settings'); */ }}
-                            >
-                                Cài đặt
-                            </button>
-                            <button
-                                className="block w-full text-left px-4 py-2 text-red-500 hover:bg-CetaceanBlue-light transition"
-                                onClick={() => handleClickLogOut()}
-                            >
-                                Đăng xuất
-                            </button>
-                        </div>
-                    )}
-                </div>
+                )}
             </nav>
         </header>
     );
